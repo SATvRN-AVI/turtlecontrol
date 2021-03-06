586064b869e7104621ce485b7d1533b2de58c3bf
SATvRN-AVI <hudson.walterf@gmail.com>
2021-03-05 20:13:51 -0600

    Did more work on WASD control

--- remotecontrol.lua
+++ remotecontrol.lua
@@ -1,30 +1,42 @@
-peripheral.find("advanced_modem").open(1)
-
-rednet.open("right")
-
-print("Use WASD and the arrow keys for direction")
 
 
-while true do
-    local event, character = os.pullEvent("char")
-    if character == "w" then
-        turtle.forward()
-        print(character .. ": Moving forward...")
-    elseif character == "s" then
-        turtle.back()
-        print(character .. ": Moving backward...")
-    elseif character == "a" then
-        turtle.turnLeft()
-        print(character .. ": Turning left...")
-    elseif character == "d" then
-        turtle.turnRight()
-        print(character .. ": Turning right...")
-    elseif key == "leftCtrl" then
-        turtle.down()
-        print(key .. ": Going down...")
-    elseif key == "space" then
-        turtle.up()
-        print(key .. ": Going down...")
-        
+
+function manualControl()
+    while true do
+        local event, character = os.pullEvent("char")
+        if character == "w" then
+            turtle.forward()
+            print(character .. ": Moving forward...")
+        elseif character == "s" then
+            turtle.back()
+            print(character .. ": Moving backward...")
+        elseif character == "a" then
+            turtle.turnLeft()
+            print(character .. ": Turning left...")
+        elseif character == "d" then
+            turtle.turnRight()
+            print(character .. ": Turning right...")
+        elseif character == "e" then
+            turtle.down()
+            print(key .. ": Going down...")
+        elseif key == "q" then
+            turtle.up()
+            print(key .. ": Going down...")
+        else
+            print("Key not recognized. Use W/A/S/D/Q/E for movmement...")
+        end
     end
 end
+
+function findModem()
+    if peripheral.find("modem").open(1) then
+        manualControl()
+    else
+        print("Please connect a modem to this device")
+    end
+end
+
+findModem()
+
+
+
