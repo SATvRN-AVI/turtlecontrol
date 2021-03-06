9486f74386b2ea3fec7c435fa567ee5d4f4d52fb
SATvRN-AVI <hudson.walterf@gmail.com>
2021-03-05 18:09:03 -0600

    Created WASD movment control

--- remotecontrol.lua
+++ remotecontrol.lua
@@ -1,34 +1,30 @@
+peripheral.find("advanced_modem").open(1)
+
 rednet.open("right")
 
 print("Use WASD and the arrow keys for direction")
 
+
 while true do
     local event, character = os.pullEvent("char")
-    -- local event, key = os.pullEvent("key")
     if character == "w" then
         turtle.forward()
         print(character .. ": Moving forward...")
-        os.sleep(0.05)
     elseif character == "s" then
         turtle.back()
         print(character .. ": Moving backward...")
-        os.sleep(0.05)
     elseif character == "a" then
         turtle.turnLeft()
         print(character .. ": Turning left...")
-        os.sleep(0.05)
     elseif character == "d" then
         turtle.turnRight()
         print(character .. ": Turning right...")
-        os.sleep(0.05)
-    --[[ elseif key == "leftCtrl" then
+    elseif key == "leftCtrl" then
         turtle.down()
         print(key .. ": Going down...")
-        os.sleep(0.05)
     elseif key == "space" then
         turtle.up()
         print(key .. ": Going down...")
-        os.sleep(0.05)
-    ]]
+        
     end
 end
